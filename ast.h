0c46ee2eac660eb7819f967ac3f5c1cc9961f3e5
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2025-10-27 20:53:00 -0300

    Alterações ast.h
    Alterações do arquivo ast.h de acordo com o material de aula do professor, versão inicial baseado no da semana , versão atual no da semana 8

--- ast.h
+++ ast.h
@@ -3,9 +3,27 @@
 
 #include "tipos.h"
 
-NoAST *criarNoNum(int valor);
-NoAST *criarNoId(char *nome);
-NoAST *criarNoOp(char operador, NoAST *esq, NoAST *dir);
-void imprimirAST(NoAST *raiz);
+typedef enum
+{
+  TIPO_INT,
+  TIPO_FLOAT,
+  TIPO_ERRO
+} Tipo;
+
+typedef struct noAST
+{
+  char operador;
+  int valor;
+  char nome[32];
+  Tipo tipo;
+  struct noAST *esquerda;
+  struct noAST *direita;
+} NoAST;
+
+NoAST *criarNoOp(char op, NoAST *esq, NoAST *dir);
+NoAST *criarNoNum(int val);
+NoAST *criarNoId(char *nome, Tipo tipo);
+void imprimirAST(NoAST *no);
+int tiposCompativeis(Tipo t1, Tipo t2);
 
 #endif
