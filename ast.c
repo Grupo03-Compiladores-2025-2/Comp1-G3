23db87bf97d5bd1e72dbd8ad957bc0325540ef2a
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2025-10-27 20:41:56 -0300

    Criação Inicial
    Criação do arquivo inicial da AST ast.c com base no material fornecido pelo professor

--- ast.c
+++ ast.c
@@ -2,3 +2,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+NoAST *criarNoNum(int valor)
+{
+  NoAST *novo = malloc(sizeof(NoAST));
+  novo->operador = ' ';
+  novo->valor = valor;
+  novo->esquerda = novo->direita = NULL;
+  return novo;
+}
+
+NoAST *criarNoId(char *nome)
+{
+  NoAST *novo = malloc(sizeof(NoAST));
+  novo->operador = ' ';
+  strcpy(novo->nome, nome);
+  novo->esquerda = novo->direita = NULL;
+  return novo;
+}
+
+NoAST *criarNoOp(char operador, NoAST *esq, NoAST *dir)
+{
+  NoAST *novo = malloc(sizeof(NoAST));
+  novo->operador = operador;
+  novo->esquerda = esq;
+  novo->direita = dir;
+  return novo;
+}
+
+void ImprimirAST(NoAST *raiz)
+{
+  if (raiz->operador == ' ')
+  {
+    printf("%d", raiz->valor);
+  }
+  else if (raiz->operador == 'i')
+  {
+    printf("%s", raiz->nome);
+  }
+  else
+  {
+    printf("(");
+    imprimirAST(raiz->esquerda);
+    printf(" %c ", raiz->operador);
+    imprimirAST(raiz->direita);
+    printf(")");
+  }
+}
